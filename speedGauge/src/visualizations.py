5b4c3f84f05a720a9b23625ad0bed63e31d48c75
Josh <ShaggyBadger@users.noreply.github.com>
2025-01-29 12:55:41 -0500

    daily update

--- speedGauge/src/visualizations.py
+++ speedGauge/src/visualizations.py
@@ -203,7 +203,7 @@ def gen_line_chart(stat_selection):
 	stat_function is either average or 
 	median
 	'''
-	if stat_selection == 'avgerage':
+	if stat_selection == 'average':
 		stat_function = statistics.mean
 	else:
 		stat_function = statistics.median
@@ -226,6 +226,10 @@ def gen_line_chart(stat_selection):
 		filtered_company_avg = stat_function(filtered_company[date])
 		raw_rtm_avg = stat_function(raw_rtm[date])
 		filtered_rtm_avg = stat_function(filtered_rtm[date])
+		print(f'{date}')
+		print(f'stat_function: {stat_function}')
+		print(f'  f_rtm_avg: {round(filtered_rtm_avg, 2)}')
+		print(f'  f_comp_avg: {round(filtered_company_avg, 2)}')
 		
 		filtered_rtm_avg_lst.append(filtered_rtm_avg)
 		filtered_company_avg_lst.append(filtered_company_avg)
@@ -245,10 +249,6 @@ def gen_line_chart(stat_selection):
 	plt.plot(dates2, filtered_rtm_avg_lst, label=rtm_label, color=settings.swto_blue, linestyle='-', linewidth=1)
 	
 	plt.plot(dates2, filtered_company_avg_lst, label=company_label, color='green', linestyle='-', linewidth=1)
-	
-	plt.plot(dates2, raw_company_avg_lst, label=f'Raw Company {stat_selection.capitalize()}', color='green', linestyle='--', linewidth=1)
-	
-	plt.plot(dates2, raw_rtm_avg_lst, label=f'Raw Rtm {stat_selection.capitalize()}', color=settings.swto_blue, linestyle='--', linewidth=1)
 	
 	# Set major locator to one tick per month
 	plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
@@ -371,37 +371,108 @@ def build_line_data(date, rtm='chris'):
 	filtered_rtm_spd = [speed for speed in rtm_spds if speed < max_spd]
 	
 	raw_comp_avg = statistics.mean(company_spds)
-	raw_rtm_avg = 
+	raw_rtm_avg = statistics.mean(rtm_spds)
 	
 	
-def build_avg_line(rtm='chris'):
+def build_line_chart(stat_selection, rtm='chris', filtered=True):
+	if stat_selection == 'average':
+		stat_function = statistics.mean
+	else:
+		stat_function = statistics.median
+	
+	dates = get_date_list()
+	dates2 = [datetime.strptime(date, '%Y-%m-%d %H:%M') for date in dates]
+	x_label = 'Date'
+	y_label = f'{stat_selection.capitalize()} Percent Speeding'
+	title = f'Historical Distribution of {stat_selection.capitalize()} Percent Speeding'
+	rtm_label = f'Rtm {stat_selection.capitalize()}'
+	company_label = f'Company {stat_selection.capitalize()}'
+	
 	conn = settings.db_connection()
 	c = conn.cursor()
 
 	# build rtm driver id list
 	rtm_id_lst = []
+	
 	sql = f'SELECT DISTINCT driver_id FROM {settings.driverInfo} WHERE rtm = ?'
 	value = (rtm,)
 	c.execute(sql, value)
 	results = c.fetchall()
 	for i in results:
-		rtm_id_lst.appens(i[0])
+		rtm_id_lst.append(i[0])
 	
-	dates = get_date_list()
 	company_line_data = []
 	rtm_line_data = []
 	
-	for date in dates:
-		company_spd_lst = []
-		rtm_spd_lst = []
+	for date in dates:	
+		sql = f'SELECT DISTINCT percent_speeding, driver_id FROM {settings.speedGaugeData} WHERE start_date = ?'
+		value = (date,)
+		c.execute(sql, value)
+		results = c.fetchall()
+		
+		company_spd_lst = [result[0] for result in results]
+		rtm_spd_lst = [result[0] for result in results if result[1] in rtm_id_lst]
+		
+		company_avg = statistics.mean(company_spd_lst)
+		company_stdev = statistics.stdev(company_spd_lst)
+		company_max = company_avg + (3 * company_stdev)
+		
+		rtm_avg = statistics.mean(rtm_spd_lst)
+		rtm_stdev = statistics.stdev(rtm_spd_lst)
+		rtm_max = rtm_avg + (3 * rtm_stdev)
+		
+		filtered_company_spd_lst = [spd for spd in company_spd_lst if spd < company_max]
+		
+		filtered_rtm_spd_lst = [spd for spd in rtm_spd_lst if spd < rtm_max]
+			
+		company_stat = round(
+			stat_function(
+				filtered_company_spd_lst
+				), 2
+			)
+		company_line_data.append(company_stat)
+		rtm_stat = round(
+			stat_function(
+				filtered_rtm_spd_lst
+				),2
+			)
+		rtm_line_data.append(rtm_stat)
 
 
+		
+
+
+	plt.figure(constrained_layout=True)
+	plt.plot(dates2, rtm_line_data, label=rtm_label, color=settings.swto_blue, linestyle='-', linewidth=3)
 	
-	plt.plot(
-		)
+	plt.plot(dates2, company_line_data, label=company_label, color='green', linestyle='-', linewidth=3)
+	
+	# Set major locator to one tick per month
+	plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+	
+	# Format the date labels to show the month and year (e.g., Jan 2024)
+	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
+	
+	# Rotate the labels for readability
+	plt.xticks(rotation=45)
+	
+	# Automatically adjust layout to avoid clipping
+	#plt.tight_layout()
+	#plt.subplots_adjust(bottom=0.15, left=0.15)  # Adjust bottom and left margins
+	
+	plt.xlabel(x_label)
+	plt.ylabel(y_label)
+	plt.title(title)
+	plt.legend()
+	
+	plt_type = f'{stat_selection.capitalize()}LineChart'
+	plt_path = save_plt(plt, dates[-1], plt_type)
+	
+	plt.show()
+	plt.close()
 		
 		
-	conm.close()
+	conn.close()
 	
 def controller(stats, rtm='chris'):
 	rtm_stats = stats['rtm']
@@ -426,14 +497,13 @@ def controller(stats, rtm='chris'):
 	
 	
 	# build line charts
-	build_avg_line()
-	gen_line_chart('avgerage')
+	#build_avg_line()
+	gen_line_chart('average')
 	gen_line_chart('median')
 	
 
 if __name__ == '__main__':
 	#prepare_line_data()
 	#temp()
-	#gen_line_chart('avgerage')
-	#gen_line_chart('median')
-	build_avg_line()
+	build_line_chart('average', rtm='chris')
+	build_line_chart('median', rtm='chris')
